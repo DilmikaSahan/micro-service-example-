88572130015da4784841148c541b136a4383611d
DilmikaSahan <153601909+DilmikaSahan@users.noreply.github.com>
2025-09-25 12:57:41 +0530

    order entity and order dto implement

--- Order/src/main/java/com/Order/Order/model/Order.java
+++ Order/src/main/java/com/Order/Order/model/Order.java
@@ -1,4 +1,19 @@
 package com.Order.Order.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Order {
+    @Id
+    private int id;
+    private int itemId;
+    private String orderDate;
+    protected int amount;
 }
